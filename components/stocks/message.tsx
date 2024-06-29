8177c50ef3d323478cb134cf3bfd951e6baeb593
ovpn-dev <ovpn05@gmail.com>
2024-06-29 11:27:34 +0100

    chai

--- components/stocks/message.tsx
+++ components/stocks/message.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { IconOpenAI, IconUser, IconRep } from '@/components/ui/icons'
+import { IconOpenAI, IconUser } from '@/components/ui/icons'
 import { cn } from '@/lib/utils'
 import { spinner } from './spinner'
 import { CodeBlock } from '../ui/codeblock'
